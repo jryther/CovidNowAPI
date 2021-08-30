a708201b5a5da75d4442fce6a6f13bc14bc093ac
Josh Ryther <jryther91@gmail.com>
2021-08-29 20:40:27 -0700

    Converted to class, added notes, and created URL builder method

--- main.py
+++ main.py
@@ -1,20 +1,76 @@
+"""
+* * Covid Interface * *
+*
+* Application used to interact with the Covid Act Now API
+* An API key is required to use the API
+* Author: Josh Ryther
+"""
+
+
 import configparser
 import requests
 import json
-import pandas as pd
 
-config = configparser.ConfigParser()
-config.read('APIFile.ini')
-key = config['API']['ApiKey']
-endpoint = config['API']['endpoint']
+class CovidInterface:
+    def __init__(self):
+        """
+        * An API key must be listed in an ini file at the root of the project
+        * The section must be labeled as 'API'
+        * The property must be labeled as 'Key'
+        """
+        self.config = configparser.ConfigParser() # Initilize ini file parser
+        self.config.read('APIFile.ini')
+        self.key = self.config['API']['Key'] # Assign API key to variable
 
-url = endpoint + 'states.json?apiKey=' + key
+    """
+    * * Create a URL for Covid Now Endpoints * *
+    * 
+    * - type must be either 'json' or 'csv'
+    * - location must be 'country', 'state', 'county', or 'cbsa'
+    * - sublocation (Leave blank if further filtering is not required):
+    *   - country: 'US"
+    *   - state: Two letter state code (example 'AK' for alaska)
+    *   - county: 5 digit FIPS code
+    *   - cbsa: cbsa code for metropolitan areas
+    * - timeseries is 'historic' if historical data is needed.  Otherwise leave blank.
+    """
+    def urlBuilder(self, type, location, sublocation = '', timeseries = ''):
+        url = 'https://api.covidactnow.org/v2/'
+        if sublocation == '':
+            if location == 'country':
+                url += ('country/US')
+            elif location == 'state':
+                url += ('states')
+            elif location == 'county':
+                url += ('counties')
+            elif location == 'state':
+                url += ('cbsas')
+        elif sublocation != '':
+            if location == 'country':
+                url += ('country/US')
+            elif location == 'state':
+                url += ('state/' + sublocation)
+            elif location == 'county':
+                url += ('county/' + sublocation)
+            elif location == 'state':
+                url += ('cbsa/' + sublocation)
+        if timeseries == 'historic':
+            url += '.timeseries'
+        url += ('.' + type)
+        url += ('?apiKey=' + self.key)
+        return url
 
-request = requests.get(url)
-response = request.text
-data = json.loads(response)
-df = pd.DataFrame.from_records(data)
-
+    """
+    * * Make a request to the Covid Now API * *
+    *
+    * A url is passed as an argument and a request is made using it.
+    * The function returns the body of the request as JSON
+    """
+    def makeRequest(self, url):
+        request = requests.get(url) # Make request to API
+        response = request.text # Retrieve body of request
+        data = json.loads(response) # Convert to JSON
+        return data
 
 """
 JSON document example passed by the API
